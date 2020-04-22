f1ae9318bbb41ee62cf66f790f78d2438d6bcc7a
Muhammad-Amer1997 <amer.rasool121@gmail.com>
2020-04-23 01:04:06 +0500

    recruiter work done

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -30,7 +30,7 @@ export class NavbarComponent implements OnInit {
 
     this.logingSerivce.loggedInUserId.subscribe(value => {
       this.companyId = value ? value : sessionStorage.getItem('companyId');
-      console.log("This is company id",this.companyId)
+     
     })
   }
 
