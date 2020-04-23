0db9b1d1225c4f082a276d571354e9db5ebd26a2
ibushah <username.ibu@gmail.com>
2020-04-23 17:58:00 +0500

    notifications counter and notifications get task done

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -4,46 +4,53 @@ import { NavbarService } from '../navbar.service';
 import { ToastrService } from 'ngx-toastr';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { BehaviorSubject, Subject } from 'rxjs';
-import {CompanyProfileComponent} from '../company-profile/company-profile.component'
+import { CompanyProfileComponent } from '../company-profile/company-profile.component'
 import { CandidateProfileComponent } from '../candidate-profile/candidate-profile.component';
 import { LoginService } from '../login-page/login.service';
+
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
- 
-  userType:any;
-  userName:any;
-  userImage:any;
+
+  userType: any;
+  userName: any;
+  userImage: any;
   userId = sessionStorage.getItem('userId');
-  companyId:any=sessionStorage.getItem('companyId');
+  notifications: Array<any> = [];
+  companyId: any = sessionStorage.getItem('companyId');
+  notificationsCount = 0;
+  notificationOpen: any;
 
 
-  
-  
-  constructor(private candP:CandidateProfileComponent,private companyProf:CompanyProfileComponent,private toastService: ToastrService, public service: ApplicantServiceService, public navbarService: NavbarService, private nzMessageService: NzMessageService,private logingSerivce:LoginService) { 
 
-   this.companyProf.logoChangeObservable.subscribe(()=> this.userImage = sessionStorage.getItem('dp'));
-   this.candP.logoChangeObservable.subscribe(()=>  this.userImage = sessionStorage.getItem('dp'))
+  constructor(private candP: CandidateProfileComponent, private companyProf: CompanyProfileComponent, private toastService: ToastrService, public service: ApplicantServiceService, public navbarService: NavbarService, private nzMessageService: NzMessageService, private logingSerivce: LoginService) {
+    this.notificationOpen = false;
+    this.companyProf.logoChangeObservable.subscribe(() => this.userImage = sessionStorage.getItem('dp'));
+    this.candP.logoChangeObservable.subscribe(() => this.userImage = sessionStorage.getItem('dp'))
 
     this.logingSerivce.loggedInUserId.subscribe(value => {
       this.companyId = value ? value : sessionStorage.getItem('companyId');
-      console.log("This is company id",this.companyId)
+      console.log("This is company id", this.companyId)
     })
   }
 
   ngOnInit(): void {
-    
+
     this.userName = sessionStorage.getItem('username');
     this.userType = sessionStorage.getItem('userType');
-    
+
     this.userImage = sessionStorage.getItem('dp');
+    if (this.companyId) {
+      this.getNotificationsCount(this.companyId);
+    }
   }
 
- 
-  getImage(){
+
+  getImage() {
 
   }
   cancel(): void {
@@ -55,9 +62,39 @@ export class NavbarComponent implements OnInit {
   }
 
 
-  goBack(){
-    
+  goBack() {
+
     window.history.go(-1);
+  }
+
+  readAllNotications() {
+    if (this.companyId) {
+      this.service.markAllNoticationsAsRead(this.companyId).subscribe((res) => {
+       
+        this.notifications = res
+      })
+    }
+  }
+
+  getNotificationsCount(companyId) {
+    this.service.getCompanyNotificationsCount(companyId).subscribe((count) => {
+      this.notificationsCount = count;
+    })
+  }
+
+  notificationOpened(isOpen) {
+
+    this.notificationOpen = !this.notificationOpen;
+
+    if (this.companyId) {
+      this.getNotificationsCount(this.companyId);
+      if (this.notificationOpen) {
+        this.service.getCompanyNotifications(this.companyId).subscribe((res) => {
+          this.notifications = res;
+          console.log(res)
+        })
+      }
+    }
   }
 
 }
