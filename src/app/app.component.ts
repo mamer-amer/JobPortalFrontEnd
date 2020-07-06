cd1a472a728846661c647d2180ffad84785e4637
ibushah <username.ibu@gmail.com>
2020-07-07 02:34:21 +0500

    chat work almost done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { ActivatedRoute, Router} from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   id = sessionStorage.getItem("userId");
-
+hidePopup=false;
+  constructor(location: Location, router: Router) {
+    router.events.subscribe((val) => {
+      if(location.path() != '' && location.path().slice(0,5)=='/chat'){
+      this.hidePopup=true;
+     
+      
+      }
+      else {
+        this.hidePopup=false;
+      }
+    });
+  }
   ngDoCheck(){
+ 
     this.id=sessionStorage.getItem("userId")
   }
 }
--- src/app/chat-pop-up-bottom/chat-pop-up-bottom.component.ts
+++ src/app/chat-pop-up-bottom/chat-pop-up-bottom.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import * as $ from 'jquery';
 import { ApplicantServiceService } from '../Services/applicant-service.service';
+import { environment } from 'src/environments/environment';
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
 @Component({
   selector: 'app-chat-pop-up-bottom',
   templateUrl: './chat-pop-up-bottom.component.html',
@@ -11,9 +14,19 @@ export class ChatPopUpBottomComponent implements OnInit {
   id = sessionStorage.getItem("userId");
   chatrooms: Array<any> = [];
   messageBoxes: Array<any> = [];
+  chats = [];
+  private stompClient;
+  // @ViewChild('scrollMe') private myScrollContainer: ElementRef;
+  scrollTop: number = null;
+  styleArray = [
+    { position: 'fixed', bottom: '0%', right: '0px' },
+    { position: 'fixed', bottom: '0%', right: '245px' },
+    { position: 'fixed', bottom: '0%', right: '490px' }
+  ]
   constructor(private service: ApplicantServiceService) { }
 
   ngOnInit(): void {
+    this.initializeWebSocketConnection()
     let that = this;
     $(".chat_content").slideToggle("slow");
     // $(".message_box").hide();s
@@ -23,29 +36,58 @@ export class ChatPopUpBottomComponent implements OnInit {
       that.getChantrooms();
     });
 
- 
-    // $(".message_header").click(function () {
-    //   $(`.message_content`).slideToggle("slow");
-    // });
-    // $(".close").click(function () {
-    //   $(".message_box").hide();
-    // });
+  }
 
-    // $(".user").click(function () {
-    //   $(".message_box").show();
-    //   $(".message_content").show();
-    //   $(".input_box").show();
-    // });
+  initializeWebSocketConnection() {
+    const url = environment.baseUrl;
+    let ws = new SockJS(url + "ws");
+    this.stompClient = Stomp.over(ws);
+    let that = this;
+    this.stompClient.connect({}, function (frame) {
 
-  
+
+
+    });
+  }
+  openGlobalSocket(chatroomId) {
+    let that = this;
+    this.stompClient.subscribe(`/topic/chatroom/${chatroomId}`, (message) => {
+
+      let chat = JSON.parse(message.body);
+      let messageBox = this.getChatroomObject(chat.chatroomId);
+
+      let chats = messageBox.chats;
+      // chats.push(chat);
+      messageBox.chats.push(chat) 
+      this.replaceArray(messageBox)
+
+
+    },{id:chatroomId});
+  }
+
+  sendMessage(messageInput, friendId, chatroomId) {
+    if (messageInput.value.length > 0) {
+
+      this.stompClient.send(`/app/chat/${friendId}/${chatroomId}`, {}, JSON.stringify({ message: messageInput.value, userId: this.id }));
+      messageInput.value = "";
+     
+      // this.messageSend = true;
+    }
 
   }
-  newChatBox(chatroom){
-    if(!this.messageBoxes.includes(chatroom))
-    this.messageBoxes.push(chatroom);
+  newChatBox(user) {
+    if (!this.contains(user.userId) && this.messageBoxes.length <= 3) {
+      // this.messageBoxes.push(chatroom);
+
+
+      this.initiateChat(user)
+      this.getChantrooms()
+
+    }
   }
-  removeMessageBox(id){
-    this.messageBoxes.splice(id,1)
+  removeMessageBox(id,chatroomId) {
+    this.stompClient.unsubscribe(chatroomId);
+    this.messageBoxes.splice(id, 1)
   }
   getChantrooms() {
     this.service.getAllChatrooms(this.id)
@@ -54,9 +96,59 @@ export class ChatPopUpBottomComponent implements OnInit {
         console.log(res)
       })
   }
-  toggle(i){
+  toggle(i, friend) {
+    console.log(friend)
+    // this.initiateChat(friend);
     $(`.mc${i}`).slideToggle("slow");
   }
+  initiateChat(user) {
+    this.service.initiateChat(this.id, user.userId)
+      .subscribe((chatroomId) => {
+        this.getAllChats(chatroomId, user)
+      })
+  }
+  getAllChats(chatroomId, user) {
+    this.service.getAllChatroomChats(chatroomId, this.id)
+      .subscribe((res) => {
+        console.log(res)
+        this.chats = res
+
+        this.openGlobalSocket(chatroomId)
+        this.messageBoxes.push({
+          chats: res,
+          user,
+          chatroomId
+        })
+   
+
+      })
+  }
+
+  contains(id) {
+    return this.messageBoxes.find((mb) => mb.user.userId == id);
+  }
+
+  getChatroomObject(chatroomId) {
+    return this.messageBoxes.find(mb => mb.chatroomId == chatroomId);
+  }
+
+  replaceArray(messageBox) {
+    this.messageBoxes.map((mb) => {
+
+      if (messageBox.chatroomId == mb.chatroomId)
+        return messageBox;
+      else
+        return mb;
+    })
+  }
+  // ngAfterViewChecked() {
+  //   this.scrollToBottom();
+  // }
+  // scrollToBottom(): void {
+  //   try {
+  //     this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
+  //   } catch (err) { }
+  // }
 }
 
 
